0b38fcc8bf851ccc56c07ae411a438e21374ceea
vvynasvane-ops <vvynasvane@gmail.com>
2026-09-13 20:36:22 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,9 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAAbPeB29qZx4H9A5RJJfnBrkMGXJLZBTA",
-    authDomain: "hostelhive-18795.firebaseapp.com",
-    projectId: "hostelhive-18795",
-    storageBucket: "hostelhive-18795.firebasestorage.app",
-    messagingSenderId: "984507880388",
-    appId: "1:984507880388:web:e6afddf66f8ef679844a91",
-    measurementId: "G-HPWJ9XEECD"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+export const firebaseConfig = {
+  apiKey: "AIzaSyAAbPeB29qZx4H9A5RJJfnBrkMGXJLZBTA",
+  authDomain: "hostelhive-18795.firebaseapp.com",
+  projectId: "hostelhive-18795",
+  storageBucket: "hostelhive-18795.firebasestorage.app",
+  messagingSenderId: "984507880388",
+  appId: "1:984507880388:web:e6afddf66f8ef679844a91",
+  measurementId: "G-HPWJ9XEECD"
+};
